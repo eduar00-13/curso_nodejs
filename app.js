1852f617221780031dd0eaa4466977608433f2ba
Brandon Eduardo Jeronimo Morales <isc18350316@gmail.com>
2021-10-26 15:20:00 -0500

    Se agrego express y dotenv

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Hola Brandon Eduardo");
+const express = require ('express');
+
+const app = express();
+
+app.get('/', function (req, res) {res.send('Hola Brandon Eduardo'); });
+
+app.listen(3000);
