26e0477578667e8c3513f2c1faf35ff3cb07a317
Brandon Eduardo Jeronimo Morales <isc18350316@gmail.com>
2021-12-11 20:13:15 -0600

    Agregado EndPoint para inicio de sesion

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -5,12 +5,25 @@ const usuariosQueries = require("../models/usuarios");
 const bcryptjs = require("bcryptjs");
 
 const usuariosGet = async (req = request, res = response) => {
+    let {limite = 10, desde = 0} = req.query; 
+
+    desde = parseInt(desde);
+    limite = parseInt(limite);
+
+            if (!Number.isInteger(limite) || !Number.isInteger(desde)){
+                res.status(400).json({msg: "No se puede realizar esta consulta"});
+                return;
+            }
+
     let conn;
 
     try {
        conn = await pool.getConnection();
 
-       const usuarios = await conn.query(usuariosQueries.selectUsuarios);
+       const usuarios = await conn.query(usuariosQueries.selectUsuarios, [
+           desde,
+           limite,
+       ]);
 
        res.json({ usuarios });
    
@@ -85,4 +98,36 @@ const usuariosDelete = async (req = request, res = response) => {
     }
 };
 
-module.exports = { usuariosGet, usuariosPost, usuariosPut, usuariosDelete };
+const usuarioSignin = async (req = request, res = response) => {
+    const {email, password} = req.body;
+    let conn;
+
+    try {
+        conn = await pool.getConnection();
+
+        const usuarios = await conn.query(usuariosQueries.getUsuarioByEmail, [email]);
+
+        if (usuarios.length === 0) {
+            res.status(404).json({msg: `No se encontro el usuario ${email}`});
+            return;
+        }
+
+        const passwordValido = bcryptjs.compareSync(password, usuarios[0].password);
+
+        if (!passwordValido){
+            res.status(401).json({msg: "La contraseña no coincide"});
+            return;
+        }
+
+        res.json({msg: "Inicio de sesion satisfactorio"});
+    } catch (error) {
+        console.log(error);
+        res
+         .status(500)
+         .json({msg: "Por favor contacte al administrador", error});
+    } finally {
+        if (conn) conn.end();
+    }
+}
+
+module.exports = { usuariosGet, usuariosPost, usuariosPut, usuariosDelete, usuarioSignin, };
